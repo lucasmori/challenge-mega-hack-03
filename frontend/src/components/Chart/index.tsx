503bde671a1c74c3cd890fd4585c97b2a47882d0
Lucas Mori <lucastecnew@gmail.com>
2020-07-04 12:37:13 -0300

    feat: initial graphic

--- frontend/src/components/Chart/index.tsx
+++ frontend/src/components/Chart/index.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Bar, Line, Pie } from 'react-chartjs-2';
 
 import './styles.css';
 
 const Chart: React.FC = () => {
-  return <div className="chart">Eu sou um chart</div>;
+  const [chartData, setChartData] = useState({
+    labels: [
+      'Boston',
+      'Worcester',
+      'Springfield',
+      'Lowell',
+      'Cambridge',
+      'New Bedford',
+    ],
+    datasets: [
+      {
+        label: 'Population',
+        data: [617594, 181045, 153060, 106519, 105162, 95072],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+          'rgba(255, 159, 64, 0.6)',
+          'rgba(255, 99, 132, 0.6)',
+        ],
+      },
+    ],
+  });
+  return (
+    <div className="chart">
+      <Bar data={chartData} />
+    </div>
+  );
 };
 
 export default Chart;
